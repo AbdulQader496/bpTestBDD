c4e83662c077d53ce8e0c111948acee53d03a94b
Abdul Qader <gm.abdul.qader@gmail.com>
2025-12-10 21:19:11 +0000

    github action runable code added

--- src/test/java/bdd/hooks/Hooks.java
+++ src/test/java/bdd/hooks/Hooks.java
@@ -2,6 +2,7 @@ package bdd.hooks;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import bdd.utils.SettingsTestData;
 import io.cucumber.java.After;
@@ -13,8 +14,25 @@ public class Hooks {
 
     @Before
     public void setup() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        // WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        // Detect if running inside GitHub Actions (Linux CI)
+        boolean isCI = System.getenv("GITHUB_ACTIONS") != null;
+
+        if (isCI) {
+            System.out.println("Running in GitHub Actions - enabling headless mode");
+
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--window-size=1920,1080");
+        }
+        // driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
+
         driver.manage().window().maximize();
         driver.get(SettingsTestData.getEnvData().getHost());
     }
